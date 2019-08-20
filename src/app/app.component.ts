f7eb2c49637e8a9915c64293b67ac36bc0d078dd
mba3gar <ramiatrouni@gmail.com>
2019-08-20 18:42:39 +0300

    Add Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+  todoForm: FormGroup;
+  todoArray: Array<string> = [];
+  sucessAlert: Boolean = false;
+  alertRemoved: Boolean = false;
+  isEdit: boolean = false;
+  isEditIndex: number = 0;
+
+  ngOnInit() {
+    this.todoForm = new FormGroup({
+      todo: new FormControl('')
+    });
+  }
+
+  submitTodo(form) {
+    this.sucessAlert = true;
+
+    this.clearBanner();
+
+    if (this.isEdit) {
+      this.todoArray[this.isEditIndex] = form.value.todo;
+      this.isEdit = false;
+      this.isEditIndex = 0;
+      return;
+    } else {
+      this.todoArray.push(form.value.todo);
+      this.todoForm.setValue({ todo: null });
+    }
+
+  }
+
+  editTodo(todo, index) {
+    this.todoForm.setValue({
+      todo: todo
+    });
+
+    this.isEdit = true;
+    this.isEditIndex = index;
+  }
+
+  clearBanner() {
+    setTimeout(() => {
+      this.sucessAlert = false;
+      this.alertRemoved = false;
+
+    }, 3000);
+  }
+
+  removeTodo(todo, index) {
+    if(this.sucessAlert){
+      this.sucessAlert = false;
+    }
+    this.alertRemoved = true;
+    this.todoArray.splice(index, 1);
+
+    this.clearBanner();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
